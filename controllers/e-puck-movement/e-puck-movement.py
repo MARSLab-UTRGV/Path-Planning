b2b2e49fae5c1854455140349a79d6e1ff38a32a
Eduardo A Cruz <eduardo.cruz03@utrgv.edu>
2023-06-15 03:09:42 -0500

    Doing changes in python

--- controllers/e-puck-movement/e-puck-movement.py
+++ controllers/e-puck-movement/e-puck-movement.py
@@ -2,13 +2,16 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+from controller import Robot, Motor, DistanceSensor, LED, Node, Device
 
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
+
+# Motor Device
+x = 1
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
--- controllers/e-puck_movement/e-puck_movement.cpp
+++ controllers/e-puck_movement/e-puck_movement.cpp
@@ -7,11 +7,48 @@
 // You may need to add webots include files such as
 // <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
 // and/or to add some other includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <webots/Device.hpp>
+#include <webots/DistanceSensor.hpp>
+#include <webots/LED.hpp>
+#include <webots/Motor.hpp>
+#include <webots/Node.hpp>
 #include <webots/Robot.hpp>
 
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
 
+int time_step;
+
+// Motor device
+static WbDeviceTag left_motor, right_motor;
+// E-puck angular speed in rad/s
+#define MAX_SPEED 6.28;
+
+//function to get simulator time step
+static int get_time_step(){
+  static int time_step = -1;
+  if (time_step == -1)
+  {
+    time_step = wb_robot_get_basic_time_step();
+  }
+  return time_step;
+}
+
+//function to set motor velocity to move forward
+static void move_forward(){
+  wb_motor_set_velocity(left_motor, MAX_SPEED);
+  wb_motor_set_velocity(right_motor, MAX_SPEED);
+}
+
+//function to init robot stuff
+static void init_robot(){
+  //get simulator time step
+  time_step = get_time_step();
+}
 // This is the main program of your controller.
 // It creates an instance of your Robot instance, launches its
 // function(s) and destroys it at the end of the execution.
